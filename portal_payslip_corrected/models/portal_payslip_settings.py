182ddf33d42166dfcd3e13f063665c080f3c9632
sob0808 <sobsinesob@gmail.com>
2025-11-26 12:09:32 +0100

    Details14

--- portal_payslip_corrected/models/portal_payslip_settings.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PortalPayslipSettings(models.Model):
-    _name='portal.payslip.settings'
-    _description='Portal Payslip Settings'
-
-    signature_attachment=fields.Many2one('ir.attachment')
-    watermark_attachment=fields.Many2one('ir.attachment')
-    notes=fields.Text()
---
+++ portal_payslip_fixed_final/__manifest__.py
@@ -0,0 +1,13 @@
+
+{
+ 'name': 'Portal Payslip Final Fixed',
+ 'version': '1.0.3',
+ 'license': 'OPL-1',
+ 'depends': ['hr_payroll','website'],
+ 'data': [
+   'views/portal_templates.xml',
+   'security/portal_security.xml',
+   'views/website_menu.xml'
+ ],
+ 'installable': True
+}
