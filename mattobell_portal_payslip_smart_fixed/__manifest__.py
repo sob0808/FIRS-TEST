2b87ec1a6d5c0a31c907245b64af7677255d3855
sob0808 <sobsinesob@gmail.com>
2025-11-26 12:44:35 +0100

    Edited1

---
+++ mattobell_portal_payslip_smart_fixed/__manifest__.py
@@ -0,0 +1,13 @@
+{
+ "name": "Mattobell Portal Payslip (Smart Fixed)",
+ "version": "1.1.1",
+ "license": "OPL-1",
+ "summary": "Portal payslip with smart email/user matching (fixed XML entities)",
+ "depends": ["hr_payroll","website"],
+ "data": [
+    "security/portal.xml",
+    "views/templates.xml",
+    "views/menu.xml"
+ ],
+ "installable": True
+}
--- mattobell_portal_payslip_smart_fixed/controllers/payslip.py
+++ mattobell_portal_payslip_smart_fixed/controllers/payslip.py
@@ -1,4 +1,3 @@
-
 from odoo import http
 from odoo.http import request
 import base64, io
@@ -25,18 +24,18 @@ class PortalPayslip(http.Controller):
         Emp=request.env['hr.employee'].sudo()
         emp=Emp.search(['|',('user_id','=',user.id),('work_email','ilike',user.email)], limit=1)
         if not emp:
-            return request.render("mattobell_portal_payslip_smart.no_employee")
+            return request.render("mattobell_portal_payslip_smart_fixed.no_employee")
 
         slips=request.env['hr.payslip'].sudo().search([('employee_id','=',emp.id)], order="date_from desc")
         Att=request.env['ir.attachment'].sudo()
         atts={s.id:Att.search([('res_model','=','hr.payslip'),('res_id','=',s.id)]) for s in slips}
-        return request.render("mattobell_portal_payslip_smart.page",{"payslips":slips,"atts":atts})
+        return request.render("mattobell_portal_payslip_smart_fixed.page",{"payslips":slips,"atts":atts})
 
     @http.route('/my/payslips/<int:sid>/download', auth='user', website=True)
     def dl(self, sid, **kw):
         slip=request.env['hr.payslip'].sudo().browse(sid)
         if not slip.exists() or not self._allowed(slip):
-            return request.render("mattobell_portal_payslip_smart.denied")
+            return request.render("mattobell_portal_payslip_smart_fixed.denied")
 
         rep=request.env.ref("hr_payroll.report_payslip")
         pdf=rep._render_qweb_pdf([slip.id])[0]
