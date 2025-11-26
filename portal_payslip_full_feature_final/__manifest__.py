1bf6ac3a8e9a3de0637bf37dc01106cb8364c70b
sob0808 <sobsinesob@gmail.com>
2025-11-26 11:17:50 +0100

    Details8

--- portal_payslip_full_feature_final/__manifest__.py
+++ portal_payslip_full_feature_final/__manifest__.py
@@ -1,19 +1,20 @@
 {
-    'name': 'Portal Payslip - SaaS Clean Strict (Final)',
-    'version': '1.0.2',
-    'summary': 'Employee payslip portal page for Odoo SaaS (strict portal auth, friendly message)',
+    'name': 'Portal Payslip - Full Feature SaaS (Auth User)',
+    'version': '1.0.0',
+    'summary': 'Employee payslip portal page (auth=user) with filters, attachments, PDF download, settings',
     'category': 'Human Resources',
-    'depends': ['hr_payroll', 'portal', 'website'],
+    'depends': ['hr_payroll','website'],
     'data': [
         'security/portal_payslip_security.xml',
         'views/portal_payslip_templates.xml',
         'views/portal_payslip_settings_views.xml',
         'views/portal_payslip_settings_action.xml',
         'views/website_menu.xml',
+        'views/portal_payslip_assets.xml',
     ],
     'assets': {
         'web.assets_frontend': [
-            '/portal_payslip_saas_clean_strict_final/static/src/css/portal_styles.css',
+            '/portal_payslip_full_feature_final/static/src/css/portal_styles.css',
         ],
     },
     'installable': True,
