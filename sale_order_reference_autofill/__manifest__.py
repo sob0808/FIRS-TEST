d6720bcb911ef1c4fbeb033b36245d8e0f50b876
sob0808 <sobsinesob@gmail.com>
2025-11-27 10:39:43 +0100

    Test

---
+++ sale_order_reference_autofill/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Sale Order Reference Autofill",
+    "version": "1.0",
+    "summary": "Automatically set the Reference field to the Sales Order Number",
+    "author": "ChatGPT",
+    "category": "Sales",
+    "depends": ["sale"],
+    "data": [],
+    "installable": True,
+    "application": False
+}
