c875f8868fbaba64c36df4b8b977be6ec0231196
sob0808 <sobsinesob@gmail.com>
2025-12-03 00:18:39 +0100

    yEAH

--- dellyman_package_management/__manifest__.py
+++ dellyman_package_management/__manifest__.py
@@ -1,20 +1,19 @@
 {
     'name': 'Dellyman Package Management',
-    'version': '1.0',
-    'summary': 'Manage packages, batches, and locations for Dellyman workflow',
-    'description': """
-        Custom package, location, and batch management workflow.
-    """,
+    'version': '1.0.0',
+    'summary': 'Manage Dellyman package batches, packages and locations',
+    'description': 'Custom package, location, and batch management workflow.',
     'author': 'Your Company',
     'category': 'Operations',
     'license': 'LGPL-3',
     'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
-        'views/package_batch_views.xml',
-        'views/package_order_views.xml',
-        'views/package_location_views.xml',
+        'data/sequence_data.xml',
         'views/menu.xml',
+        'views/package_batch_views.xml',
+        'views/package_location_views.xml',
+        'views/package_order_views.xml'
     ],
     'installable': True,
     'application': True,
