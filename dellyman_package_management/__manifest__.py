a0391a84d9f12b5e890f180058c56ca2865786eb
sob0808 <sobsinesob@gmail.com>
2025-12-03 01:03:51 +0100

    Yes002

--- dellyman_package_management/__manifest__.py
+++ dellyman_package_management/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Dellyman Package Management',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'summary': 'Manage Dellyman package batches, packages and locations',
     'description': 'Custom package, location, and batch management workflow.',
     'author': 'Your Company',
--- dellyman_package_management/models/package_batch.py
+++ dellyman_package_management/models/package_batch.py
@@ -1,18 +1,20 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class PackageBatch(models.Model):
     _name = "package.batch"
     _description = "Package Batch"
     _rec_name = "name"
 
-    name = fields.Char(required=True, default='New')
-    sequence = fields.Char(string='Batch Sequence', readonly=True)
+    name = fields.Char(string='Batch Number', required=True, readonly=True, default='New')
+    sequence = fields.Char(string='Sequence', readonly=True)
     arrival_date = fields.Date(default=fields.Date.context_today)
     package_ids = fields.One2many('package.order', 'batch_id', string='Packages')
 
-    @api.model
-    def create(self, vals):
-        if vals.get('name', 'New') == 'New':
-            vals['sequence'] = self.env['ir.sequence'].next_by_code('package.batch') or '/'
-            vals['name'] = vals.get('name', vals['sequence'])
-        return super(PackageBatch, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('name','New') == 'New':
+                seq = self.env['ir.sequence'].next_by_code('package.batch') or '/'
+                vals['sequence'] = seq
+                vals['name'] = vals.get('name', seq)
+        return super(PackageBatch, self).create(vals_list)
